b1c58cd998c59564e865df1b3168b354df41f7f9
Himanshu Patil <142036432+Himanshuu1508@users.noreply.github.com>
2025-11-12 03:21:08 +0530

    Update DAA1_fibonacci.py

--- DAA1_fibonacci.py
+++ DAA1_fibonacci.py
@@ -1,5 +1,7 @@
-def fibonacci_iterative(n:int):
-    if n == 1:
+def fibonacci_iterative(n: int):
+    if n <= 0:
+        return "Invalid input! Enter n >= 1"
+    elif n == 1:
         return 0
     elif n == 2:
         return 1
@@ -7,12 +9,15 @@ def fibonacci_iterative(n:int):
         dp = [0] * n
         dp[0] = 0
         dp[1] = 1
-        for i in range(2,n):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n-1]
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n - 1]
 
-def fibonacci_recursive(n):
-    if n == 1:
+
+def fibonacci_recursive(n: int):
+    if n <= 0:
+        return "Invalid input! Enter n >= 1"
+    elif n == 1:
         return 0
     elif n == 2:
         return 1
@@ -20,11 +25,12 @@ def fibonacci_recursive(n):
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
 
-n = int(input("Enter value of n(nth Fibonacci number): "))
-print(f"Fibonacci Number(Iterative): {fibonacci_iterative(n)}")
-print(f"Fibonacci Number(Recursive): {fibonacci_recursive(n)}")
+n = int(input("Enter value of n (nth Fibonacci number): "))
 
-print("\nFibonacci Series (Iterative): ", end="") #O(n) and o(n) space 
+print(f"Fibonacci Number (Iterative): {fibonacci_iterative(n)}")
+print(f"Fibonacci Number (Recursive): {fibonacci_recursive(n)}")
+
+print("\nFibonacci Series (Iterative): ", end="") #O(n) time and O(n) space
 for i in range(1, n + 1):
     print(fibonacci_iterative(i), end=" ")
 
