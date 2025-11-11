3e8a17681109d286d3ccb916198e79b29a9a7ddb
Himanshu Patil <142036432+Himanshuu1508@users.noreply.github.com>
2025-11-12 03:09:23 +0530

    Add files via upload

---
+++ DAA1_fibonacci.py
@@ -0,0 +1,33 @@
+def fibonacci_iterative(n:int):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        dp = [0] * n
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]
+
+def fibonacci_recursive(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+n = int(input("Enter value of n(nth Fibonacci number): "))
+print(f"Fibonacci Number(Iterative): {fibonacci_iterative(n)}")
+print(f"Fibonacci Number(Recursive): {fibonacci_recursive(n)}")
+
+print("\nFibonacci Series (Iterative): ", end="") #O(n) and o(n) space 
+for i in range(1, n + 1):
+    print(fibonacci_iterative(i), end=" ")
+
+print("\nFibonacci Series (Recursive): ", end="") #O(2^n) time and O(n) space
+for i in range(1, n + 1):
+    print(fibonacci_recursive(i), end=" ")
